b7e07ecd898ed6d2cbcdb4e3f3bba60434d0c3dc
xxxzz25 <34541124+xxxzz25@users.noreply.github.com>
2020-08-10 07:18:35 +0000

    Deploying to gh-pages from master 4cebf56f40f670279e1ff3080c4a87ee65546271

---
+++ precache-manifest.4a8d90a6889e8026a876bad4e4234651.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3da29ae4c6ca57568274dc908f8c0a8",
+    "url": "/cicd-test/index.html"
+  },
+  {
+    "revision": "e4908dd827aec607d3c3",
+    "url": "/cicd-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f72929c62d871284dbf",
+    "url": "/cicd-test/static/js/2.b140c238.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cicd-test/static/js/2.b140c238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4908dd827aec607d3c3",
+    "url": "/cicd-test/static/js/main.6a9b5407.chunk.js"
+  },
+  {
+    "revision": "9793366645939f8c73ea",
+    "url": "/cicd-test/static/js/runtime-main.d5c6c799.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cicd-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
