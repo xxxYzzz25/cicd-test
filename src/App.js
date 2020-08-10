4cebf56f40f670279e1ff3080c4a87ee65546271
Chris <34541124+xxxzz25@users.noreply.github.com>
2020-08-10 15:16:54 +0800

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://klearthink.com"
           target="_blank"
           rel="noopener noreferrer"
         >
